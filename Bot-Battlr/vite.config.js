d31fc1ed968f64ca433cd80e4e6ea623157a4aec
Gideon kipkoech <gideon.kipkoech1@student.moringaschool.com>
2025-04-24 22:28:14 +0300

    this is my commit after testing

--- Bot-Battlr/vite.config.js
+++ Bot-Battlr/vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+if (!globalThis.crypto) {
+  const crypto = await import('crypto');
+  globalThis.crypto = {
+    getRandomValues: (arr) => crypto.webcrypto.getRandomValues(arr),
+  };
+}
 export default defineConfig({
   plugins: [react()],
 })
+// vite.config.js
+
+
+
