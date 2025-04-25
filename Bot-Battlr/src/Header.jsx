e7912aeb1f298a690a36e3c746034122171b8cff
Gideon kipkoech <gideon.kipkoech1@student.moringaschool.com>
2025-04-25 10:51:22 +0300

    this is my last commit

--- Bot-Battlr/src/Header.jsx
+++ Bot-Battlr/src/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div id="header">
       <h1>Bot Battlr</h1>
-      <p>Build your own bot army!</p>
+      <p>Build your own custom bot army!</p>
       
     </div>
   );
